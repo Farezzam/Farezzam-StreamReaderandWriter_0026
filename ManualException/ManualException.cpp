b1e3cb241c0cc0d5c3010e41cc41706ab86e06fc
ASUS <faris13abiyu@gmail.com>
2024-06-04 18:41:57 +0700

    int main ManualException

---
+++ ManualException/ManualException.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	try {
+		cout << "Selamat Belajar di Prodi TI UMY" << endl;
+		// throw 0,5; // Melemparkan sebuah integer maka
+		cout << "Pernyataan tidak akan dieksekusi" << endl;
+	}
+	catch (int a) {
+		// Blok ini akan dieksekusi
+		cout << "Pengecualian akan dieksekusi" << endl;
+	}
+	catch (...) {
+		/*Jika selain integer maka blok ini akan dieksekusi*/
+		cout << "default Pengecualian dieksekusi" << endl;
+	}
+
+	return 0;
+}
